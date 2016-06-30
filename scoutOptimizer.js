9a1663550db5c61d6a47b1a271cf27cb6e4be2d5
ChrisAB <cristianbatrin@gmail.com>
2016-06-30 13:05:52 +0300

    Update scoutOptimizer.js

--- scoutOptimizer.js
+++ scoutOptimizer.js
@@ -1,1 +1,13 @@
-test
+var GLEVEL = document.getElementById('glevel');
+var LEVEL = document.getElementById('level');
+var XP = document.getElementById('xp');
+var EFF = document.getElementById('eff');
+var KRES = document.getElementById('kres');
+var KLUCK = document.getElementById('kluck');
+var RELIC = document.getElementById('relic');
+var RINVESTED = document.getElementById('rinvested');
+var GINVESTED = document.getElementById('ginvested');
+
+div#scout.onlick=function(){
+  
+}
