6352fb0b557ee82986520d60fa862de5e31c1339
ChrisAB <cristianbatrin@gmail.com>
2016-06-30 23:13:50 +0300

    Update scoutOptimizer.js

--- scoutOptimizer.js
+++ scoutOptimizer.js
@@ -19,12 +19,12 @@ $(document).ready(function() {
         var VALUE_BEFORE = RINVESTED*RELICP+GINVESTED*GEMP ;
         var VALUE_AFTER = (((XP/0,2)*(XP/0,2 + 1))/2)*RELICP;
         function Current() {
-            var EFF_LEVEL= (100+LEVEL+EFF+RRES)/100;
-            var T1 = (EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
-            var T2 = (EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
-            var T3 = (EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
-            var T4 = (EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
-            var T5 = (EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
+            var EFF_LEVEL= (LEVEL+EFF+RRES)/100;
+            var T1 = (1+EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
+            var T2 = (1+EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
+            var T3 = (1+EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
+            var T4 = (1+EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
+            var T5 = (1+EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
             var CT1 = (0.90+KLUCK/100+RLUCK/100);
             if(CT1>1){CT1=1;}
             var CT2 = (0.60+(KLUCK/100+RLUCK/100)*(Math.pow(0.67,1)));
@@ -38,12 +38,12 @@ $(document).ready(function() {
             return (T1*CT1+T2*CT2+T3*CT3+T4*CT4+T5*CT5);
         }
         function RelicRes() {
-            var EFF_LEVEL= (100+LEVEL+EFF+RRES+1,5)/100;
-            var T1 = (EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
-            var T2 = (EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
-            var T3 = (EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
-            var T4 = (EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
-            var T5 = (EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
+            var EFF_LEVEL= (LEVEL+EFF+RRES+1,5)/100;
+            var T1 = (1+EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
+            var T2 = (1+EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
+            var T3 = (1+EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
+            var T4 = (1+EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
+            var T5 = (1+EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
             var CT1 = (0.90+KLUCK/100+RLUCK/100);
             if(CT1>1){CT1=1;}
             var CT2 = (0.60+(KLUCK/100+RLUCK/100)*(Math.pow(0.67,1)));
@@ -57,12 +57,12 @@ $(document).ready(function() {
             return (T1*CT1+T2*CT2+T3*CT3+T4*CT4+T5*CT5);
         }
         function GemRes() {
-            var EFF_LEVEL= (100+LEVEL+EFF+RRES)/100;
-            var T1 = (EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
-            var T2 = (EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1.01+(GRES/100))*2;
-            var T3 = (EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1,01+(GRES/100))*4;
-            var T4 = (EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1.01+(GRES/100))*5;
-            var T5 = (EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1.01+(GRES/100))*10;
+            var EFF_LEVEL= (LEVEL+EFF+RRES)/100;
+            var T1 = (1+EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
+            var T2 = (1+EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1.01+(GRES/100))*2;
+            var T3 = (1+EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1,01+(GRES/100))*4;
+            var T4 = (1+EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1.01+(GRES/100))*5;
+            var T5 = (1+EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1.01+(GRES/100))*10;
             var CT1 = (0.90+KLUCK/100+RLUCK/100);
             if(CT1>1){CT1=1;}
             var CT2 = (0.60+(KLUCK/100+RLUCK/100)*(Math.pow(0.67,1)));
@@ -77,12 +77,12 @@ $(document).ready(function() {
         }
         function ResLuck() {
             NRLUCK=RLUCK+0,3;
-            var EFF_LEVEL= (100+LEVEL+EFF+RRES)/100;
-            var T1 = (EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
-            var T2 = (EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
-            var T3 = (EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
-            var T4 = (EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
-            var T5 = (EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
+            var EFF_LEVEL= (LEVEL+EFF+RRES)/100;
+            var T1 = (1+EFF_LEVEL)*(1+(1/KRES))*(1+(1/GRES));
+            var T2 = (1+EFF_LEVEL*(Math.pow(0.67,1)))*(1+(KRES/100))*(1+(GRES/100))*2;
+            var T3 = (1+EFF_LEVEL*(Math.pow(0.67,2)))*(1+(KRES/100))*(1+(GRES/100))*4;
+            var T4 = (1+EFF_LEVEL*(Math.pow(0.67,3)))*(1+(KRES/100))*(1+(GRES/100))*5;
+            var T5 = (1+EFF_LEVEL*(Math.pow(0.67,4)))*(1+(KRES/100))*(1+(GRES/100))*10;
             var CT1 = (0.90+KLUCK/100+NRLUCK/100);
             if(CT1>1){CT1=1;}
             var CT2 = (0.60+(KLUCK/100+NRLUCK/100)*(Math.pow(0.67,1)));
