0bc713a64e5456f7de7585a4a9678deddb55d18c
ChrisAB <cristianbatrin@gmail.com>
2016-06-30 13:12:33 +0300

    Update scoutOptimizer.js

--- scoutOptimizer.js
+++ scoutOptimizer.js
@@ -11,3 +11,10 @@ var GINVESTED = document.getElementById('ginvested');
 document.getElementById("scout").onclick = function(){
     document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
 }
+
+window.onclick = myFunction;
+
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
+}
