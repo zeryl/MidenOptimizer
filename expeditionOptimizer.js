0a28da00cb55ef8316213213deee93f977cf7d52
ChrisAB <cristianbatrin@gmail.com>
2016-07-02 14:57:31 +0300

    Create expeditionOptimizer.js

---
+++ expeditionOptimizer.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $("#expedition").click(function() {
+  //Mining
+    var T1M = parseInt($('#t1m').val());
+    var T1M = parseInt($('#t1m').val());
+    var T1M = parseInt($('#t1m').val());
+    var T1M = parseInt($('#t1m').val());
+    var T1M = parseInt($('#t1m').val());
+    //Gathering
+    var T1G = parseInt($('#t1g').val());
+    var T1G = parseInt($('#t1g').val());
+    var T1G = parseInt($('#t1g').val());
+    var T1G = parseInt($('#t1g').val());
+    var T1G = parseInt($('#t1g').val());
+    //Woodcutting
+    var T1W = parseInt($('#t1w').val());
+    var T1W = parseInt($('#t1w').val());
+    var T1W = parseInt($('#t1w').val());
+    var T1W = parseInt($('#t1w').val());
+    var T1W = parseInt($('#t1w').val());
+    //Fishing
+    var T1F = parseInt($('#t1f').val());
+    var T1F = parseInt($('#t1f').val());
+    var T1F = parseInt($('#t1f').val());
+    var T1F = parseInt($('#t1f').val());
+  });
+});
