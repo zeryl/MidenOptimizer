bafb68e0124f9f8035c0ce11360dd308e8be9857
ChrisAB <cristianbatrin@gmail.com>
2016-07-02 18:35:01 +0300

    Update expeditionOptimizer.js

--- expeditionOptimizer.js
+++ expeditionOptimizer.js
@@ -68,19 +68,85 @@ $(document).ready(function() {
           switch(Math.min(T1M,T1G,T1W,T1F)){
             case T1M:
               $("#t1m").css("color", "green");
+              break;
+            case T1G:
+              $("#t1g").css("color", "green");
+              break;
+            case T1W:
+              $("#t1w").css("color", "green");
+              break;
+            case T1F:
+              $("#t1f").css("color", "green");
+              break;
           }
           break;
         case 2:
           HEROES[i]=Math.min(T2M,T2G,T2W,T2F);
+          switch(Math.min(T2M,T2G,T2W,T2F)){
+            case T2M:
+              $("#t2m").css("color", "green");
+              break;
+            case T2G:
+              $("#t2g").css("color", "green");
+              break;
+            case T2W:
+              $("#t2w").css("color", "green");
+              break;
+            case T2F:
+              $("#t2f").css("color", "green");
+              break;
+          }
           break;
         case 3:
           HEROES[i]=Math.min(T3M,T3G,T3W,T3F);
+          switch(Math.min(T3M,T3G,T3W,T3F)){
+            case T3M:
+              $("#t3m").css("color", "green");
+              break;
+            case T3G:
+              $("#t3g").css("color", "green");
+              break;
+            case T3W:
+              $("#t3w").css("color", "green");
+              break;
+            case T3F:
+              $("#t3f").css("color", "green");
+              break;
+          }
           break;
         case 4:
           HEROES[i]=Math.min(T4M,T4G,T4W,T4F);
+          switch(Math.min(T4M,T4G,T4W,T4F)){
+            case T4M:
+              $("#t4m").css("color", "green");
+              break;
+            case T4G:
+              $("#t4g").css("color", "green");
+              break;
+            case T3W:
+              $("#t4w").css("color", "green");
+              break;
+            case T4F:
+              $("#t4f").css("color", "green");
+              break;
+          }
           break;
         case 5:
           HEROES[i]=Math.min(T5M,T5G,T5W,T5F);
+          switch(Math.min(T5M,T5G,T5W,T5F)){
+            case T5M:
+              $("#t5m").css("color", "green");
+              break;
+            case T5G:
+              $("#t5g").css("color", "green");
+              break;
+            case T5W:
+              $("#t5w").css("color", "green");
+              break;
+            case T5F:
+              $("#t5f").css("color", "green");
+              break;
+          }
           break;
       }
       switch()
